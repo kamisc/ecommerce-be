9a3b343028d06e0a74872da71f6ac87447492181
kamisc <kamil.seweryn@o2.pl>
2023-05-07 22:07:00 +0200

    add cross origin and open app for frontend

--- src/main/java/com/sewerynkamil/ecommerce/dao/ProductRepository.java
+++ src/main/java/com/sewerynkamil/ecommerce/dao/ProductRepository.java
@@ -2,6 +2,8 @@ package com.sewerynkamil.ecommerce.dao;
 
 import com.sewerynkamil.ecommerce.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin("${allowed.page}")
 public interface ProductRepository extends JpaRepository<Product, Long> {
 }
