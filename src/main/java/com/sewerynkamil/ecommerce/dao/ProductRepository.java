303ced75191d9bf1c8e63c17519f9b1f5cb58186
kamisc <kamil.seweryn@o2.pl>
2023-05-10 22:48:47 +0200

    add query method to find products by category id

--- src/main/java/com/sewerynkamil/ecommerce/dao/ProductRepository.java
+++ src/main/java/com/sewerynkamil/ecommerce/dao/ProductRepository.java
@@ -1,9 +1,13 @@
 package com.sewerynkamil.ecommerce.dao;
 
 import com.sewerynkamil.ecommerce.entity.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 @CrossOrigin("${allowed.page}")
 public interface ProductRepository extends JpaRepository<Product, Long> {
+    Page<Product> findByCategoryId(@Param("id") final Long id, final Pageable pageable);
 }
